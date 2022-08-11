47c17bfcc5657df38ac520dc4eba1fa589042944
KaoHu <kaohulogist@gmail.com>
2022-08-10 23:26:43 -0700

    update info

--- DigitalCV-app.py
+++ DigitalCV-app.py
@@ -41,7 +41,7 @@ with st.container():
   left, right = st.columns([4,1])
   with left:    
     st.info('''
-    Dedicated Electrical Engineer recent graduate with project experience in design and development of the system.
+    Dedicated recent graduate Electrical and Computer Engineer  with project experience in design and development of the system.
     A fast and attentive learner candidate who enjoys being part of a team to support the development of engineering projects.
     ''')
   with right:
@@ -149,10 +149,11 @@ st.markdown('''
 
 txt2_41('**Vinny AC, Houston, TX** *(AC Technician Associate)*','06/2021 - present')
 st.markdown('''
-- Diagnosed and repaired electrical and mechanical faults to home HVAC systems.
-- Operated equipment installations with safe work habits when working in a team environment.
-- Tested the systematic product to follow standard NEC concepts, practices, and procedures.
-- Provided excellent communication guidance to clients on maintaining applications, increasing contracts by 20%.
+-   Diagnosed and repaired electrical and mechanical faults to home HVAC systems.
+-   Operated equipment installations with safe work habits when working in a team environment.
+-   Experienced with electrical equipment and motors.
+-   Tested the systematic product to follow standard NEC concepts, practices, and procedures.
+-   Increased contracts by 20% by providing excellent communication guidance to clients on maintaining applications and preventing potential problems.
 ''')
 
 txt2_41('**NASA Texas Space Grant Consortium Design Challenge, Houston, TX**','09/2021 - 06/2022')
@@ -161,40 +162,59 @@ txt2_41('*(HEC5 Team – Technical Design System Lead)*','')
 st.markdown('''
 -	Designed and implemented a system of interchangeable controllers to interact with multiple sensors (analog & digital) for automation performing the appropriate program accordingly to a positional identifier.
 -	Extracted data to store in a database. Displayed share data virtually via a GUI application by Python programming.
+-   Worked with team members to ensure appropriate skills are addressing and incorporating accordingly. 
+-   Performed electrical wiring, modifications, and upgrades to the design.
 -	Collaborated with team members to assign suitable role skills, resulting in timely completion within budgetary $1000 limitations. Monitored and controlled the project via Git.
+-   Communicated feedback from test components, sub-systems, codes, and wiring connections to determine the failures.
+-   Trained team members for the system's technical performance, including electrical repair and inspection.
 -	Tested components, sub-systems, codes, and wiring connections to determine the failures.
 -	Generated AutoCAD 3D modeling and applied 3D printing to complete prototype.
 -	Achieved a highly reliable product with better than 90% success for each interchangeable swapping run.
--	Experienced with electrical testing equipment voltmeters, oscilloscopes, power supply, soldering, and silicone seal for technical troubleshooting and designing an electric control system.
+-	Experienced with electrical testing equipment voltmeters, oscilloscopes, power supply, soldering, and silicone seal with detailed troubleshooting of electrical systems and designing an electric control system.
 ''')
 
 txt2_41('**U.S. Department of Commerce, Katy, TX** *(IT Support)*','05/2020 - 11/2020')
 st.markdown('''
--	Provided technical assistance to users via Citrix Workspace, Remedy, and Information Admission tools.
+-	Provided technical assistance to users via IT ticketing environment Citrix Workspace, Remedy.
 -	Coordinated with each manager of a group of 25 persons to install remote updates to prevent service disruption and resolve technical issues.
 -	Applied information security to monitor account credentials.
-- Recorded tickets issue to evaluate the issues for escalation to a higher level.
+-   Recorded tickets issue to evaluate the issues for escalation to a higher level.
 -	Performed work with a proficiency level in MS Office, Outlook, PowerPoint, OneNote, Word, Excel, and Outlook. 
 ''')
 
 txt2_41('**US Jaclean, Stafford, TX** *(Manager)*','05/2016 – 09/2019')
 st.markdown('''
 -	Managed the Houston brand office’s day-to-day operations in vending, retail, wholesale, and delivery.
--	Organized teams to meet deadlines, and distribute products from warehouse to stores. Validated non-working massage chair units for replacements or repairs for hardware and software. 
--	Documented resources management to record, update, and document to regional manager.
+-	Diagnosed electrical problems on non-working massage chairs for both hardware and software to repair issues or replacement.  
+-   Communicated with repair technicians to identify high repeat repair items.
+-	Documented resources management to record, update, and document directly to regional manager.
 -	Controlled and distributed warehouse duties including operating fork lifts, power, and hand tools within occupational hazards and safety precautions.
 -	Performed problem solving and troubleshooting in electrical and mechanical building maintenance.
+-   Collaborateded on product quality between supplier and vendor to deliver better quality to customers.
+-   Ensured scheduled for electrical repairs of working equipment and diagnostic tools during the down time.
 
 ''')
 
 txt2_41('**9c Studio, Planet Earth** *(Cinematography Freelance)*','2017 - present')
 st.markdown('''
-- Building a freelance business on customized fine art storytelling wedding videos from cinematic visual elements: lighting, camera setup, color, edit.
-- Prioritized responsibility for the integrity of locations with flexible hours available to clients.
-- Organized securely audio and video equipment inventory after every event.
--	Experienced to set up, operate and tear down of audio and video equipment in live events.
+-   Building a freelance business on customized fine art storytelling wedding videos from cinematic visual elements: lighting, camera setup, color, edit.
+-   Prioritized responsibility for the integrity of locations with flexible hours available to clients.
+-   Organized securely audio and video equipment inventory after every event.
+-   Experienced to set up, operate and tear down of audio and video equipment in live events.
 ''')
 
+#####################
+st.markdown('''
+## Relevant Coursework
+#### *PLC & HMI development*
+-   Developed PLC and HMI programs using standard methods and structured approaches with TIA Portal V17.
+-   Applied PLC program writing to HMI development under Siemens’s flagship development environment.
+-   Familiar with lockout/tagout (LOTO) safety manufacturing environment.
+#### *Electric Power Systems*
+-   Developed and solved 1-phase and 3-phase circuits for current, voltage, and power.
+-	Possessed an educational background to understand the knowledge of working on AC and DC motors.
+-	Analyzed the power transformer of the step-up and step-down circuit models.
+''')
 
 #####################
 st.markdown('''
